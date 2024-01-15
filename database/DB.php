4ecb127f850d9fb0e65ab38da9e898dbc94e6589
METR1CKA <ferchosalazar054@gmail.com>
2024-01-15 09:46:09 -0600

    [UPD] v2.6
    - Final

--- database/DB.php
+++ database/DB.php
@@ -13,9 +13,9 @@ class DB
     $dsn = "mysql:host=$this->host;dbname=$this->db";
 
     $options = [
-      PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+      PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
       PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-      PDO::ATTR_EMULATE_PREPARES   => false,
+      PDO::ATTR_EMULATE_PREPARES => false,
     ];
 
     try {
@@ -63,7 +63,7 @@ class DB
   public function getFavoritos()
   {
     try {
-      $sql = "SELECT * FROM favoritos";
+      $sql = "SELECT * FROM favoritos ORDER BY id DESC";
 
       $stmt = $this->pdo->prepare($sql);
 
